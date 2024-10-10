6dbe0b866ed6b986a234a8d1f9479dece0a0d040
Ndrea-gitHub <andreadgsan@gmail.com>
2024-10-10 22:53:28 +0000

    Improved name and age display

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 
-let name= document.getElementById('name');
-let age= document.getElementById('age');
+let names= document.getElementById('name');
+let ageInput= document.getElementById('age');
 let button= document.querySelector('button');
 let greeting= document.createElement('p');
 let response= document.createElement('p');
 
 
-greeting.textContent= 'Hello lets see what your elgible for...' ;
 
 //Remember the ON SUBMIT 
 
@@ -14,7 +13,8 @@ greeting.textContent= 'Hello lets see what your elgible for...' ;
 
 button.addEventListener('click',function(){
     document.body.appendChild(greeting);
-
+ let age= parseInt(ageInput.value);
+ greeting.textContent= `Hello ${names.value} lets see what you are eligible for`;
     if (age>=0){
         if(age>=18){
             response.textContent='You are eligible to vote and drive';
@@ -22,6 +22,7 @@ button.addEventListener('click',function(){
             document.body.appendChild(response);
         }
         else if (age>=16){
+            console.log(names.value);
             response.textContent= 'You are eligible for a learners permit but not to vote';
             response.style.color='blue';
             document.body.appendChild(response);
@@ -37,7 +38,12 @@ button.addEventListener('click',function(){
         response.textContent= 'Please enter a positive integer';
             document.body.appendChild(response);
     }
-
 })
 
-
+const loc_ion= ()=>{
+const locations=['Pensylvania ','Philadelphia'];
+const[s,c]=locations;
+console.log(s);
+console.log(c);
+};
+loc_ion();
